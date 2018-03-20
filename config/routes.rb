00feeb411c7cb7461942354392bba07dc83c3d4f
Maria Elvira <medevuono@gmail.com>
2018-03-20 16:19:55 -0500

    Implements logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:new, :create]
   resources :products
