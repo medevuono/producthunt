9511dbdc39449059585c7008fc847949f9bbf275
Maria Elvira <medevuono@gmail.com>
2018-03-20 15:34:35 -0500

    Implements login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   resources :users, only: [:new, :create]
+  resources :products
   #get '/products', to: 'products#index'
   #get '/products/new', to: 'products#new', as: 'new_product'
   #post '/products', to: 'products#create'
@@ -8,8 +15,7 @@ Rails.application.routes.draw do
   #patch '/products/:id', to: 'products#update'
   #delete '/products/:id', to: 'products#destroy'
 
-  resources :products
-
   root 'products#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
